20bd3e5f064fc7b64fb46830355d9d4586b15368
TJReidel <thomasreidel@me.com>
2026-08-31 15:59:10 +0200

    Update PWA cache for adherence v2 assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="pillplan-v3";
-const SHELL=["/","/index.html","/manifest.json"];
+const CACHE="pillplan-v4";
+const SHELL=["/","/index.html","/manifest.json","/adherence-v2.js","/adherence-v2-adapter.js"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
