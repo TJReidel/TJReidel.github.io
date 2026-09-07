1141e9ecdf03cefd22934c3868313e2759714865
TJReidel <thomasreidel@me.com>
2026-09-07 10:22:16 +0200

    PillPlan: refresh PWA cache after safety gate update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="pillplan-v12-historyfix4";
+const CACHE="pillplan-v12-safetytest1";
 const SHELL=["/index.html","/manifest.json","/adherence-v2.js","/adherence-v2-adapter.js","/pillplan-v12-correction-ui.js","/medication-schedule-v1.js","/statistics-v2.js","/icon.png","/icon-512.png"];
 
 self.addEventListener("install",e=>{
