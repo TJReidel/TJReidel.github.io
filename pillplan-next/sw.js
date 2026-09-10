4037830024d03c2916eac42aad5386bc766bb079
TJReidel <thomasreidel@me.com>
2026-09-10 23:34:18 +0200

    PillPlan: cache UX completion release

--- pillplan-next/sw.js
+++ pillplan-next/sw.js
@@ -1,5 +1,5 @@
-const CACHE='pillplan-next-v19-timezones';
-const ASSETS=['/pillplan-next/','/pillplan-next/index.html','/pillplan-next/core-v4.js','/pillplan-next/timezone-v1.js','/pillplan-next/design-master-v4.css','/pillplan-next/design-master.css','/pillplan-next/manifest.json','/icon.png','/icon-512.png'];
+const CACHE='pillplan-next-v20-ux-completion';
+const ASSETS=['/pillplan-next/','/pillplan-next/index.html','/pillplan-next/core-v4.js','/pillplan-next/timezone-v1.js','/pillplan-next/ux-completion-v1.js','/pillplan-next/design-master-v4.css','/pillplan-next/design-master.css','/pillplan-next/manifest.json','/icon.png','/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{const r=e.request,u=new URL(r.url);if(r.mode==='navigate'||u.pathname.startsWith('/pillplan-next/')){e.respondWith(fetch(r,{cache:'no-store'}).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(r,copy));return res;}).catch(()=>caches.match(r)||caches.match('/pillplan-next/index.html')));return;}if(u.origin===self.location.origin){e.respondWith(caches.match(r).then(c=>c||fetch(r).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put(r,copy));return res;})));}});
