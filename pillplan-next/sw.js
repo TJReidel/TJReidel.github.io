d0490e3713fdcd41694374208c45c9b7d6c8c2ae
TJReidel <thomasreidel@me.com>
2026-09-10 22:16:51 +0200

    PillPlan: bump cache for intake removal safeguard

--- pillplan-next/sw.js
+++ pillplan-next/sw.js
@@ -1,4 +1,4 @@
-const CACHE='pillplan-next-v8-longitudinal';
+const CACHE='pillplan-next-v9-intake-delete-guard';
 const ASSETS=['/pillplan-next/','/pillplan-next/index.html','/pillplan-next/app.js','/pillplan-next/longitudinal.js','/pillplan-next/enhancements.js','/pillplan-next/longitudinal-finalize.js','/pillplan-next/manifest.json','/icon.png','/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
