017bc200168669d11aa8d91bfb0a4c1e42eaa524
TJReidel <thomasreidel@me.com>
2026-09-10 20:21:46 +0200

    Refresh cache for multiple daily intake support

--- pillplan-next/sw.js
+++ pillplan-next/sw.js
@@ -1,4 +1,4 @@
-const CACHE='pillplan-next-v3-classic';
+const CACHE='pillplan-next-v4-multitime';
 const ASSETS=['/pillplan-next/','/pillplan-next/index.html','/pillplan-next/app.js','/pillplan-next/manifest.json','/icon.png','/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
